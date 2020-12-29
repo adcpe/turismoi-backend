5923d4d84ba9379e618cf719d478e6282cdd7bcd
Andrés Del Carpio <andresdc@andresdc.net>
2020-12-29 13:10:34 -0500

    Added GetData service

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,4 +1,5 @@
 class ToursController < ApplicationController
   def index
+    @tours = GetData.call
   end
 end
---
+++ app/services/get_data.rb
@@ -0,0 +1,34 @@
+require 'http'
+require 'json'
+
+class GetData < ApplicationService
+  def initialize
+    @key = ENV['APIKEY']
+    @tour_data = []
+    @activities = {}
+
+    get_data
+  end
+
+  def call
+    [@tour_data, @activities]
+  end
+
+  private
+
+  def get_data
+    response = JSON.parse(
+      HTTP.auth(@key).
+          get('https://api.turismoi.com/api/tours').
+          body
+    )
+
+    # get attributes from each tour, push it to @tour_data
+    response['data'].each { |el| @tour_data << el['attributes'] }
+
+    # populate @activities as: :id = 'name'
+    response['included'].each do |el|
+      @activities[el['attributes']['id'].to_sym] = el['attributes']['name']
+    end
+  end
+end
