6b1dc6bc3b00ed69cdc6cc392e5f79864232a81e
Andrés Del Carpio <andresdc@andresdc.net>
2020-12-30 12:34:41 -0500

    Added Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: [
+    './app/**/*.html',
+    './app/**/*.erb',
+    './app/**/*.slim',
+    './app/**/*.vue',
+    './app/**/*.jsx',
+  ],
+  theme: {},
+  variants: {},
+  plugins: [],
+};
